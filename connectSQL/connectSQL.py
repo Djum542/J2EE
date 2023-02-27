1849429e1460db1a0687a7cdc6086aee3d777897
Djum542 <djum54022@gmail.com>
2023-02-27 14:57:03 +0700

    GUI cellphone

---
+++ connectSQL/connectSQL.py
@@ -0,0 +1,22 @@
+import pymysql.cursors
+# Kết nối vào database.
+connection = pymysql.connect(host='127.0.0.1',
+                             user='root',
+                             password='15142',
+                             db='cellphone',
+                             charset='utf8',
+                             cursorclass=pymysql.cursors.DictCursor)
+print ("connect successful!!")
+try:
+    with connection.cursor() as cursor:
+        # SQL
+        sql = "SELECT * FROM cellphone.profilecell; "
+        # Thực thi câu lệnh truy vấn (Execute Query).
+        cursor.execute(sql)
+        print ("cursor.description: ", cursor.description)
+        print()
+        for row in cursor:
+            print(row)
+finally:
+    # Đóng kết nối (Close connection).
+    connection.close()
